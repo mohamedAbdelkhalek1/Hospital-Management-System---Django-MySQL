78c832beb56cea309b6f21034d38441871da4dda
Mohammed Abd Elkhalek <120330120+mohamedAbdelkhalek1@users.noreply.github.com>
2024-02-08 20:54:00 +0200

    Update password resete urls

--- master/urls.py
+++ master/urls.py
@@ -16,10 +16,10 @@ urlpatterns = [
     
     path('change_password/', views.change_password_view, name='change_password'),
     
-    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name= "password_reset_form.html"),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name = 'password_reset_sent.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'password_reset_confirm.html'),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name = 'password_reset_complete.html'),name='password_reset_complete'),
     
     path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
     path('doctor_dashboard/', views.doctor_dashboard_view, name='doctor_dashboard'),
